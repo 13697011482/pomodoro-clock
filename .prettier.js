640576c371e69865b62ec79dd6d7268265398b49
chentianyao <2236297997@qq.com>
2022-10-28 11:51:25 +0800

    feat: init

---
+++ .prettier.js
@@ -0,0 +1,23 @@
+/*
+ * @FilePath: .prettier.js
+ * @Author: chentianyao
+ * @Date: 2022-10-28 11:19:42
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2022-10-28 11:19:44
+ * Copyright: 2022 xxxTech CO.,LTD. All Rights Reserved.
+ * @Descripttion: 
+ */
+
+module.exports = {
+  printWidth: 140,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  arrowParens: 'always',
+  insertPragma: false,
+  requirePragma: false,
+  useTabs: false
+};
